f5c3836e1f68d8a4fe90da30abbf73bf05a14074
antonbelev <belevanton@gmail.com>
2014-02-24 17:32:22 +0000

    Mycampus service is not returning dummy data. Added test classes for each user story.

--- Project/src/attributes/MyCampusService.java
+++ Project/src/attributes/MyCampusService.java
@@ -1,6 +1,9 @@
 package attributes;
 
+import java.util.Date;
 import java.util.HashMap;
+
+import enums.SessionEnum;
 
 public class MyCampusService {
 	
@@ -23,8 +26,25 @@ public class MyCampusService {
 	public static MyCampusService getSingleton() {
 		if (myCampuseInstance == null) {
 			myCampuseInstance = new MyCampusService();
+			importMyCampusData();
 		}
 		return myCampuseInstance;
+	}
+
+	private static void importMyCampusData() {
+		Course c1 = new Course("PSD3", new HashMap<String, Student>(), new HashMap<String, Session>());
+		Room r1 = new Room("bo720");
+		HashMap<String, Course> studentCourses = new HashMap<String, Course>();		
+		studentCourses.put("PSD3", c1);
+		Student s1 = new Student("1", "Pancho Durvodelski", studentCourses);
+		HashMap<String, Student> sessionStudents = new HashMap<String, Student>();	
+		sessionStudents.put(s1.getName(), s1);
+		
+		@SuppressWarnings("deprecation")
+		Session sess1 = new Session("Lab1", new Date(2013, 2, 24), new Date(2013, 3, 31), sessionStudents, r1, true, SessionEnum.WEEKLY);
+		
+		c1.getSessions().put(sess1.getSessionName(), sess1);
+		c1.getStudentsEnrolled().put(s1.getName(), s1);
 	}
 	
 	
